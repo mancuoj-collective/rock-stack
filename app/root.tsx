a1b449cb45172913932562ea924c759a9379fa69
mancuoj <mancuoj@gmail.com>
2024-03-31 01:42:54 +0800

    feat: tailwind support

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,16 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from '@remix-run/react'
+import type { LinksFunction } from '@remix-run/node'
+import stylesheet from '~/tailwind.css?url'
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: stylesheet },
+]
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,25 +6,13 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer">
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/jokes" rel="noreferrer">
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="h-dvh flex items-center justify-center">
+      <h1 className="text-3xl font-serif text-slate-700">
+        Welcome to{' '}
+        <a target="_blank" href="https://remix.run/docs" rel="noreferrer" className="hover:underline">
+          Remix
+        </a>
+      </h1>
     </div>
   )
 }
