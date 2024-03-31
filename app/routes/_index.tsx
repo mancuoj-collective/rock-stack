fc301cd5e9da1fa62ddd27280ca790f7a6281545
mancuoj <mancuoj@gmail.com>
2024-03-31 21:19:03 +0800

    revert: "feat: update homepage"
    This reverts commit 924af595fd26df698ae8ed761fccde6bc87117b1.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,3 @@
-import { Link } from '@nextui-org/react'
 import type { MetaFunction } from '@remix-run/node'
 
 export const meta: MetaFunction = () => {
@@ -7,19 +6,12 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="flex h-dvh items-center justify-center">
-      <h1 className="font-serif text-3xl">
+    <div className="h-dvh flex items-center justify-center">
+      <h1 className="text-3xl font-serif text-slate-700">
         Welcome to{' '}
-        <Link
-          className="text-3xl"
-          href="https://remix.run/docs"
-          color="foreground"
-          underline="hover"
-          isExternal
-          showAnchorIcon
-        >
+        <a target="_blank" href="https://remix.run/docs" rel="noreferrer" className="hover:underline">
           Remix
-        </Link>
+        </a>
       </h1>
     </div>
   )
