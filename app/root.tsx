cee26e479985bd98a927a522a8fbe8ce0f4e2886
mancuoj <mancuoj@gmail.com>
2024-04-11 21:46:08 +0800

    feat: new favicon

--- app/root.tsx
+++ app/root.tsx
@@ -5,7 +5,14 @@ import stylesheet from '~/styles/globals.css?url'
 
 export const meta: MetaFunction = () => [{ title: 'Rock Stack' }]
 
-export const links: LinksFunction = () => [{ rel: 'stylesheet', href: stylesheet }]
+export const links: LinksFunction = () => [
+  {
+    rel: 'icon',
+    type: 'image/svg+xml',
+    href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🪨</text></svg>',
+  },
+  { rel: 'stylesheet', href: stylesheet },
+]
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
