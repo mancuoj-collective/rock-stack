ababcb82f78e249696ae2f6facd12d7f4a2dc365
mancuoj <mancuoj@gmail.com>
2024-04-13 15:14:31 +0800

    chore: update deps

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 🤘 Remix Rock Stack
+
+- [Remix](https://remix.run/)
+- [Fly.io](https://fly.io/) + GitHub Actions
+- [Tailwind CSS](https://tailwindcss.com/)
+- [shadcn/ui](https://github.com/shadcn/ui)
+- [Prettier](https://prettier.io/)
+- ...
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,7 +7,7 @@ export default function Index() {
     <div className="relative size-full bg-white bg-dot-black/[0.2] dark:bg-black dark:bg-dot-white/[0.2]">
       <div className="pointer-events-none absolute inset-0 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black" />
       <div className="flex h-dvh flex-col items-center justify-center gap-5">
-        <h1 className="font-serif text-3xl font-bold">Welcome to Remix</h1>
+        <h1 className="font-serif text-3xl font-bold">Welcome to Rock Stack</h1>
         <div className="flex gap-3">
           <Button asChild>
             <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
